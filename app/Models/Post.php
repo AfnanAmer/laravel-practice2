6f9878197d4a5d5456b39a3aaceb468d823bfa8a
Afnan Amer <AfnanAmirAltalhi@gmail.com>
2022-07-15 23:05:01 +0300

    filter 0

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,13 +23,22 @@ class Post extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    protected function scopeFilter($query, array $filter){
+    protected function scopeFilter($query, array $filter)
+    {
         // ويش ذا الزين!!
-        $query->when($filter['search'] ?? false, function($query,$search){ 
-            
+        $query->when($filter['search'] ?? false,  fn ($query, $search) =>
+
         $query
-                ->where('title', 'like', '%'  . $search .'%')
-                ->orWhere('body', 'like', '%' . $search . '%');
-      });
+            ->where('title', 'like', '%'  . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%'));
+
+
+        $query->when($filter['category'] ?? false,fn ($query, $category) =>
+            $query
+                ->whereExist(fn ($query) =>
+                $query->from('categories')
+                    ->where('categories.id', 'posts.category_id')
+                    ->where('categories.slug', $category))
+        );
     }
 }
